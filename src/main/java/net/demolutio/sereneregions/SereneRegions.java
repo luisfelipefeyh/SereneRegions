64e8630c6fd3651dd773375318b66e82079fab94
luisfelipefeyh <luisfelipe@feyh.com.br>
2026-08-06 11:14:55 -0300

    Delete useless boilerplate

--- src/main/java/net/demolutio/sereneregions/SereneRegions.java
+++ src/main/java/net/demolutio/sereneregions/SereneRegions.java
@@ -34,22 +34,12 @@ public class SereneRegions
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
-
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
-        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
     {
         // Some common setup code
         LOGGER.info("HELLO FROM COMMON SETUP");
-
-        if (Config.logDirtBlock)
-            LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
-
-        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
-
-        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
